bec37ea8f60044c67f9c04a2e6d8a42afb89a678
Ashley Ginsberg <aginsberg@mdlive.com>
2015-11-01 21:12:18 -0500

    Add #all to Citizens to create an array of all citizens

--- lib/citizen.rb
+++ lib/citizen.rb
@@ -1,13 +1,19 @@
 class Citizen
   attr_accessor :name
-  attr_reader :id
+  attr_reader :id, :citizens
 
   @@serial = 1
+  @@citizens = []
 
   def initialize(name: name=nil)
     @name = name
     @id = @@serial
     @@serial += 1
+    @@citizens << self
+  end
+
+  def self.all
+    @@citizens
   end
 
   def ask_for_name
--- spec/citizen_spec.rb
+++ spec/citizen_spec.rb
@@ -2,6 +2,11 @@ require 'rspec'
 require 'citizen'
 
 RSpec.describe Citizen do
+  before(:each) do
+    @@serial = 1
+    @@citizens = []
+  end
+
   it 'has an id' do
     citizen1 = Citizen.new(name: 'Ashley')
     citizen2 = Citizen.new(name: 'Matt')
@@ -15,5 +20,18 @@ RSpec.describe Citizen do
     result = citizen.name
 
     expect(result).to eq ('Ashley')
+  end
+
+  it '#all lists all citizens' do
+    citizen1 = Citizen.new(name: 'Ashley')
+    citizen2 = Citizen.new(name: 'Matt')
+    citizen3 = Citizen.new(name: 'Josh')
+
+    result = Citizen.all
+
+    expect(result.length).to eq (3)
+    expect(result[0].name).to eq ('Ashley')
+    expect(result[1].name).to eq ('Matt')
+    expect(result[2].name).to eq ('Josh')
   end
 end
